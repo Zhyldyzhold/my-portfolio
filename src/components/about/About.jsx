b8cf68538b04a041a10bf9fda2f53436f8fb949d
Zhyldyz <jyldyzkg@gmail.com>
2023-03-14 13:57:23 +0600

    add new photo

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import ME from "../../assets/me-about.png";
+import ME from "../../assets/menew.jpg";
 import { FaAward, FaUsers, FaFolderOpen } from "react-icons/fa";
 
 const About = () => {
